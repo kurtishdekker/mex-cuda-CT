3a422fe26695e00d14b6b4ee585fbfa62ce1457c
Kurtis Dekker <kdekker@kingstonhsc.ca>
2019-07-25 08:39:20 -0400

    added compile-all script, removed individual compile scripts

---
+++ compile.m
@@ -0,0 +1,75 @@
+% ENVIRONMENT VARIABLES AND PATHS
+cuda_path = getenv('CUDA_PATH');
+matlab_path = [matlabroot '\extern\include'];
+vs_path = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\' %this will need to be changed for your installation of visual studio
+gpu_compute = '-gencode=arch=compute_61,code=sm_61'; %change the numbers for your specific GPU compute capability (i.e. compute capability 5.2 -> 52, etc.)
+user_matlab_path = [userpath '\mex-cuda-CT'];
+
+% compile the various tools
+
+%% Forward and Backprojection Operators
+cd .\projection_kernels
+
+%% Compile the CUDA code into object files (.o)
+% forward projection operations
+nvccCommandLine = ['nvcc -c CUDAmex_FP.cu -o CUDAmex_FP.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% general3D forward projection operations
+nvccCommandLine = ['nvcc -c CUDAmex_general3D_FP.cu -o CUDAmex_general3D_FP.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% backprojection operations
+nvccCommandLine = ['nvcc -c CUDAmex_BP.cu -o CUDAmex_BP.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% forward projection operations
+nvccCommandLine = ['nvcc -c CUDAmex_general3D_BP.cu -o CUDAmex_general3D_FP.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% compile the object files into .mex files
+mex('-largeArrayDims','CUDAmex_FP.o', ['-L' cuda_path],'-lcudart',['-outdir ' user_matlab_path]);	
+mex('-largeArrayDims','CUDAmex_BP.o', ['-L' cuda_path],'-lcudart',['-outdir ' user_matlab_path]);	
+mex('-largeArrayDims','CUDAmex_general3D_FP.o', ['-L' cuda_path],'-lcudart',['-outdir ' user_matlab_path]);	
+mex('-largeArrayDims','CUDAmex_general3D_BP.o', ['-L' cuda_path],'-lcudart',['-outdir ' user_matlab_path]);	
+
+% clean up .o files
+delete *.o
+
+%% OSC-TV iterative Reconstruction
+cd ..\iterative_reconstruction
+% forward projection operations
+nvccCommandLine = ['nvcc -c CUDAmex_oscIter.cu -o CUDAmex_oscIter.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% TV-minimization
+nvccCommandLine = ['nvcc -c CUDAmex_TVmin_3D.cu -o CUDAmex_TVmin_3D.o -O3 ' gpu_compute ' --ptxas-options=-v -I"' matlab_path '" -l"' vs_path '\lib" -ccbin "' vs_path '\bin"'];
+status = system(nvccCommandLine);
+if status < 0
+	error 'Error invoking nvcc'
+end
+
+% compile the object files into .mex files
+mex('/NODEFAULTLIB:MSVCRT.lib','-largeArrayDims', 'CUDAmex_oscIter.o', ['-L' cuda_path], '-lcudart',['-outdir ' user_matlab_path]);
+mex('/NODEFAULTLIB:MSVCRT.lib','-largeArrayDims', 'CUDAmex_oscIter.o', ['-L' cuda_path], '-lcudart',['-outdir ' user_matlab_path]);
+
+% clean up .o files
+delete *.o
+
+%% add compiled mex functions to MATLAB path
+addpath(user_matlab_path); savepath;
